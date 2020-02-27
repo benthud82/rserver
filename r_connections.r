453fec435745cf93e952a2e18425c87fb33b9c87
Hudson <Bentley.Hudson@henryschein.com>
2020-02-27 09:42:50 -0600

    add files

---
+++ r_connections.r
@@ -0,0 +1,6 @@
+#NY Server
+#mychannel <- dbConnect(MySQL(), user="bhudson", pass="dave41", host="USNYMESLOT1")
+#Local
+#mychannel <- dbConnect(MySQL(), user="root", pass="dave41", host="127.0.0.1")
+#google
+mychannel <- dbConnect(MySQL(), user="bentley", pass="dave41", host="104.154.153.225")
